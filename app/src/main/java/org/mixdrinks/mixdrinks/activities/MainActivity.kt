e1de1ed93870e03805c342370f71cc8367dca199
yura <yura.zb.ua@gmail.com>
2023-01-23 12:12:47 +0200

    added binding

---
+++ app/src/main/java/org/mixdrinks/mixdrinks/activities/MainActivity.kt
@@ -0,0 +1,16 @@
+
+package org.mixdrinks.mixdrinks.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.mixdrinks.mixdrinks.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
