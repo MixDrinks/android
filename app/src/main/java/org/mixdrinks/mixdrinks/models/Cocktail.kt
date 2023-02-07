c7f59cbb9d99107a29071bfdaa47c87263b64566
yura <yura.zb.ua@gmail.com>
2023-02-07 14:49:03 +0200

    fixed

--- app/src/main/java/org/mixdrinks/mixdrinks/models/Cocktail.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/models/Cocktail.kt
@@ -3,9 +3,11 @@ package org.mixdrinks.mixdrinks.models
 import com.google.gson.annotations.SerializedName
 
 data class Cocktail (
+
     @SerializedName("id"         ) var id         : Int?              = null,
     @SerializedName("name"       ) var name       : String?           = null,
     @SerializedName("rating"     ) var rating     : Float?              = null,
     @SerializedName("visitCount" ) var visitCount : Int?              = null,
     @SerializedName("images"     ) var images     : ArrayList<Image> = arrayListOf()
+
 )
