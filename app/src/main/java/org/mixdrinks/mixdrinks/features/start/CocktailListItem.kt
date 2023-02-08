ef49df9dcaa39220fb1b3e84493c4c6ca32a453e
yura <yura.zb.ua@gmail.com>
2023-02-08 10:45:46 +0200

    code refuctoring

--- app/src/main/java/org/mixdrinks/mixdrinks/features/start/CocktailListItem.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/features/start/CocktailListItem.kt
@@ -1,4 +1,5 @@
 package org.mixdrinks.mixdrinks.features.start
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
@@ -14,16 +15,18 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import org.mixdrinks.mixdrinks.features.start.data.Cocktail
-import org.mixdrinks.mixdrinks.features.start.data.DataImage
+import org.mixdrinks.mixdrinks.data.Cocktail
+import org.mixdrinks.mixdrinks.data.DataImage
 import coil.compose.AsyncImage
 
 @Composable
 fun CocktailListItem(item: Cocktail) {
     Card(modifier = Modifier.padding(5.dp)) {
         Column {
-            Header(item.rating, item.visitCount)
-            Image(item.images.first())
+            ItemHeader(item.rating, item.visitCount)
+
+            // запитати по отримані зображень з API
+            ItemImage(item.images.first())
             item.name?.let {
                 Text(text = it, style = typography.h6, modifier = Modifier.align(Alignment.CenterHorizontally))
             }
@@ -32,7 +35,7 @@ fun CocktailListItem(item: Cocktail) {
 }
 
 @Composable
-private fun Image(image: DataImage) {
+private fun ItemImage(image: DataImage) {
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -51,7 +54,7 @@ private fun Image(image: DataImage) {
 }
 
 @Composable
-private fun Header(rating: Float? = null, visitCount: Int? = null) {
+private fun ItemHeader(rating: Float? = null, visitCount: Int? = null) {
     Row(
         modifier = Modifier
             .fillMaxSize()
@@ -61,28 +64,24 @@ private fun Header(rating: Float? = null, visitCount: Int? = null) {
         verticalAlignment = Alignment.CenterVertically
     ) {
         rating?.let {
-            Image(
-                painter = painterResource(id = R.drawable.ic_baseline_star_24),
-                contentDescription = null,
-                modifier = Modifier.size(20.dp)
-            )
-            Text(
-                text = it.toString().substring(0, 3),
-                modifier = Modifier.padding(start = 5.dp, end = 7.dp)
-            )
+            ItemHeaderImage(R.drawable.ic_baseline_star_24, it.toString().substring(0, 3))
+            Spacer(modifier = Modifier.width(10.dp))
         }
         visitCount?.let {
-            Image(
-                painter = painterResource(id = R.drawable.ic_baseline_remove_red_eye_24),
-                contentDescription = null,
-                modifier = Modifier.size(20.dp)
-            )
-            Text(
-                text = it.toString(),
-                modifier = Modifier.padding(start = 5.dp)
-            )
+            ItemHeaderImage(R.drawable.ic_baseline_remove_red_eye_24, it.toString())
         }
-
     }
+}
 
+@Composable
+private fun ItemHeaderImage(imageId: Int, text: String) {
+    Image(
+        painter = painterResource(id = imageId),
+        contentDescription = null,
+        modifier = Modifier.size(20.dp)
+    )
+    Text(
+        text = text,
+        modifier = Modifier.padding(start = 5.dp)
+    )
 }
