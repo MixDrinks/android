cd2e9a95f2767b22faad175c81292ccfc0653913
yura <yura.zb.ua@gmail.com>
2023-02-09 09:21:13 +0200

    code refactoring

--- app/src/main/java/org/mixdrinks/mixdrinks/features/start/ui/MainViewModel.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/features/start/ui/MainViewModel.kt
@@ -11,7 +11,7 @@ import org.mixdrinks.mixdrinks.app.RetrofitClient
 import org.mixdrinks.mixdrinks.features.start.data.Cocktail
 import org.mixdrinks.mixdrinks.features.start.data.CocktailProvider
 
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
     var cocktailListResponse: List<Cocktail> by mutableStateOf(listOf())
     private val cocktailProvider = RetrofitClient.retrofit.create(CocktailProvider::class.java)
 
