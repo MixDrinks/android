fdef838a2f1f530492224fd94c20563b9c2da213
yura <yura.zb.ua@gmail.com>
2023-02-22 14:13:12 +0200

    created data class

---
+++ app/src/main/java/org/mixdrinks/mixdrinks/features/data/Tag.kt
@@ -0,0 +1,6 @@
+package org.mixdrinks.mixdrinks.features.data
+
+data class Tag(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/org/mixdrinks/mixdrinks/features/data/Tool.kt
@@ -0,0 +1,9 @@
+package org.mixdrinks.mixdrinks.features.data
+
+data class Tool(
+    val amount: Int,
+    val id: Int,
+    val images: List<DataImage>,
+    val name: String,
+    val unit: String
+)
