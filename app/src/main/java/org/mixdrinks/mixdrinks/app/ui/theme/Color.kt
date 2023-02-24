fe1e3efc620491536e812d3e280dfe1035591a07
yura <yura.zb.ua@gmail.com>
2023-02-24 13:58:14 +0200

    changed colors, theme, font

--- app/src/main/java/org/mixdrinks/mixdrinks/app/ui/theme/Color.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/app/ui/theme/Color.kt
@@ -4,5 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val Green700 = Color(0XFF2B4718)
+val Green = Color(0XFF4e6640)
+val Black18 = Color(0XFF181818)
+
--- app/src/main/java/org/mixdrinks/mixdrinks/app/ui/theme/Type.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/app/ui/theme/Type.kt
@@ -1,18 +1,52 @@
 package org.mixdrinks.mixdrinks.app.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.mixdrinks.mixdrinks.R
+
+private val appFontFamily: FontFamily = FontFamily(
+    Font(R.font.source_sans_pro_regular, FontWeight.Normal),
+    Font(R.font.source_sans_pro_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = appFontFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
+        fontSize = 18.sp,
+        color = Green700
+    ),
+    body2 = TextStyle(
+        fontSize = 14.sp,
+        color = Green700
+    ),
+    h1 = TextStyle(
+        fontWeight = FontWeight.W700,
+        fontSize = 32.sp,
+        color = Color.Black
+    ),
+    h2 = TextStyle(
+        fontWeight = FontWeight.W700,
+        fontSize = 24.sp,
+        color = Black18
+    ),
+    h3 = TextStyle(
+        fontWeight = FontWeight.W600,
+        fontSize = 18.sp,
+        color = Color.Black
+    ),
+    h4 = TextStyle(
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        color = Green700
     )
+
     /* Other default text styles to override
     button = TextStyle(
         fontFamily = FontFamily.Default,
