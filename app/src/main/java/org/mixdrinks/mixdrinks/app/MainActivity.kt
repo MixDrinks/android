f399d277f7205f84482f9dc7125fa4efc0b44616
yura <yura.zb.ua@gmail.com>
2023-02-24 13:52:21 +0200

    added navcontroller

--- app/src/main/java/org/mixdrinks/mixdrinks/app/MainActivity.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/app/MainActivity.kt
@@ -1,11 +1,16 @@
 package org.mixdrinks.mixdrinks.app
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import org.mixdrinks.mixdrinks.app.ui.theme.MixDrinksTheme
+import org.mixdrinks.mixdrinks.features.detail.ui.DetailScreen
 import org.mixdrinks.mixdrinks.features.start.ui.StartScreen
 import org.mixdrinks.mixdrinks.features.start.ui.StartScreenViewModel
 
@@ -14,13 +19,39 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            MixDrinksTheme {
-                Log.d("MyLog", "MixDrinksTheme")
-                StartScreen(mainViewModel.cocktailListResponse)
+            MixDrinksApp(modifier = Modifier, mainViewModel = mainViewModel)
+        }
+    }
+}
+@Composable
+fun MixDrinksApp(
+    modifier: Modifier = Modifier,
+    mainViewModel: StartScreenViewModel = StartScreenViewModel(),
+) {
+    MixDrinksTheme {
+        val navController = rememberNavController()
+
+        NavHost(
+            navController = navController,
+            startDestination = "start"
+        ) {
+            composable("start")
+            {
+                StartScreen(
+                    modifier = modifier,
+                    mainViewModel.cocktailListResponse,
+                    onNavigateToDetail = { navController.navigate("coctail/$it") }
+                )
                 mainViewModel.getCocktail(0)
             }
+            composable("coctail/{coctailId}") {
+                backStackEntry ->
+                    backStackEntry.arguments?.getString("coctailId")
+                    DetailScreen(modifier = modifier)
+            }
+
+
         }
     }
 }
