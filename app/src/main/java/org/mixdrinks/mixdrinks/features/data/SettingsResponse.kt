5e383a6a66e0652f17562ebbc7e65a8107926348
yura <yura.zb.ua@gmail.com>
2023-02-24 14:41:00 +0200

    code refactoring

--- app/src/main/java/org/mixdrinks/mixdrinks/features/data/SettingsResponse.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/features/data/SettingsResponse.kt
@@ -3,7 +3,7 @@ package org.mixdrinks.mixdrinks.features.data
 import com.google.gson.annotations.SerializedName
 
 class SettingsResponse(
-    @SerializedName("minVote") val minVote: Int? = null,
-    @SerializedName("maxVote") val maxVote: Int? = null,
-    @SerializedName("pageSize") val pageSize: Int? = null
+    val minVote: Int? = null,
+    val maxVote: Int? = null,
+    val pageSize: Int? = null
 )
