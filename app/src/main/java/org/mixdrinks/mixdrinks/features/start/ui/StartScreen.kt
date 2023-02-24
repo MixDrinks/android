d9369f2bf02f0c9c12adfa46745df0b7fcc77805
yura <yura.zb.ua@gmail.com>
2023-02-24 13:53:54 +0200

    added navcontroller

--- app/src/main/java/org/mixdrinks/mixdrinks/features/start/ui/StartScreen.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/features/start/ui/StartScreen.kt
@@ -1,20 +1,23 @@
 package org.mixdrinks.mixdrinks.features.start.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import org.mixdrinks.mixdrinks.features.start.data.Cocktail
+import org.mixdrinks.mixdrinks.features.data.Cocktail
 @Composable
-fun StartScreen(cocktail: List<Cocktail>) {
+fun StartScreen(modifier: Modifier, cocktail: List<Cocktail>, onNavigateToDetail: (id: Int) -> Unit) {
     LazyColumn(
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-    ) {
+        contentPadding = PaddingValues(horizontal = 8.dp, vertical = 8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        ) {
         items(
             items = cocktail,
             itemContent = {
-                CocktailListItem(item = it)
+                CocktailListItem(item = it, modifier = modifier, onClickAction =  onNavigateToDetail )
             }
         )
     }
