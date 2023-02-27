adfdc2073f20e877d062d7ca79e39f543cb1c022
yura <yura.zb.ua@gmail.com>
2023-02-27 15:00:19 +0200

    added function getting cocktail by id

--- app/src/main/java/org/mixdrinks/mixdrinks/features/data/CocktailProvider.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/features/data/CocktailProvider.kt
@@ -7,8 +7,10 @@ import retrofit2.http.Query
 interface CocktailProvider {
     @GET("search/cocktails")
     suspend fun getCocktails(@Query("page") p: Int): CocktailsResponse
-}
 
+    @GET("cocktails/full")
+    suspend fun getCoktail(@Query("id") id: Int): DetailCocktailResponse
+}
 
 object CocktailProviderMock {
     private val jsonString = """{
