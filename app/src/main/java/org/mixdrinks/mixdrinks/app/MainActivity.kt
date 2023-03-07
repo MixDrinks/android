3617cf89154b816b16c343b4211c8f4bafd1f02c
yura <yura.zb.ua@gmail.com>
2023-03-07 10:29:52 +0200

    code refactoring

--- app/src/main/java/org/mixdrinks/mixdrinks/app/MainActivity.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/app/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
@@ -12,7 +11,6 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import org.mixdrinks.mixdrinks.app.ui.theme.MixDrinksTheme
 import org.mixdrinks.mixdrinks.features.detail.ui.DetailScreen
-import org.mixdrinks.mixdrinks.features.header.ui.HeaderScreen
 import org.mixdrinks.mixdrinks.features.start.ui.StartScreen
 import org.mixdrinks.mixdrinks.features.start.ui.StartScreenViewModel
 
@@ -39,15 +37,12 @@ fun MixDrinksApp(
         ) {
             composable("start")
             {
-                Column {
-                    HeaderScreen(modifier = modifier)
-                    StartScreen(
-                        modifier = modifier,
-                        mainViewModel.cocktailListResponse,
-                        onNavigateToDetail = { navController.navigate("cocktail/$it") }
-                    )
-                    mainViewModel.getCocktail(0)
-                }
+                StartScreen(
+                    modifier = modifier,
+                    mainViewModel.cocktailListResponse,
+                    onNavigateToDetail = { navController.navigate("cocktail/$it") }
+                )
+                mainViewModel.getCocktail(0)
 
             }
             composable("cocktail/{cocktailId}") {
