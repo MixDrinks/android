7b65c4a938e46abd89cb6595b980a197d9fe2016
yura <yura.zb.ua@gmail.com>
2023-03-13 19:57:16 +0200

    code refactoring

--- app/src/main/java/org/mixdrinks/mixdrinks/app/MainActivity.kt
+++ app/src/main/java/org/mixdrinks/mixdrinks/app/MainActivity.kt
@@ -3,13 +3,12 @@ package org.mixdrinks.mixdrinks.app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MixDrinksApp(modifier = Modifier)
+            MixDrinksApp()
         }
     }
 }
